e43b24f2dce124525d838a908347fa32ec702235
iolivares <4794288+iolivares@users.noreply.github.com>
2025-06-27 21:08:16 -0400

    Add syslog traces and improve error messages

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <syslog.h>
+#include <errno.h>
 
 int main(int argc, char const *argv[]) {
+    openlog("writer", LOG_PID, LOG_USER);
+
     if (argc < 3) {
-        // @todo replace with syslog
-        printf("Usage: %s <file_path> <content>\n", argv[0]);
+        syslog(LOG_ERR, "Usage: %s <file_path> <content>", argv[0]);
         return 1;
     }
 
@@ -17,28 +19,27 @@ int main(int argc, char const *argv[]) {
     int fd;
     fd = open(file_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd == -1) {
-        // @todo replace with syslog
-        printf("Error opening file %s for writing\n", file_path);
+        syslog(LOG_ERR, "Error: opening file %s for writing: %s", file_path, strerror(errno));
         return 1;
     }
 
-    printf("Writing %s to %s\n", content, file_path);
+    syslog(LOG_DEBUG, "Writing %s to %s", content, file_path);
 
     int result = 0;
 
     ssize_t nr;
     nr = write(fd, content, strlen(content));
     if (nr == -1) {
-        // @todo replace with syslog
-        printf("Error writing to file %s\n", file_path);
+        syslog(LOG_ERR, "Error: writing to file %s: %s", file_path, strerror(errno));
         result = 1;
     }
 
     if (close(fd) == -1) {
-        // @todo replace with syslog
-        printf("Error closing file %s\n", file_path);
+        syslog(LOG_ERR, "Error: closing file %s: %s", file_path, strerror(errno));
         return 1;
     }   
+
+    closelog();
 
     return result;
 }
