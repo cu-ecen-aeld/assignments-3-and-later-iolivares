0bdde80d8b16967292826304e6d4373b2f7545da
iolivares <4794288+iolivares@users.noreply.github.com>
2025-07-20 00:06:28 +0000

    Build kernel and copy image to outdir

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -5,14 +5,20 @@
 set -e
 set -u
 
-OUTDIR=/tmp/aeld
+# Use https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git for the linux kernel source directory. 
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+# Kernel branch/tag
 KERNEL_VERSION=v5.15.163
+
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
+
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 
+# Take a single argument outdir which is the location on the filesystem where the output files should be placed.
+# If not specified, your script should use /tmp/aeld as outdir
+OUTDIR=/tmp/aeld
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
@@ -21,23 +27,38 @@ else
 	echo "Using passed directory ${OUTDIR} for output"
 fi
 
-mkdir -p ${OUTDIR}
+#  Create a directory outdir if it doesn’t exist.  Fail if the directory could not be created.
+if ! mkdir -p ${OUTDIR}; then
+    echo "Error: could not create output directory ${OUTDIR}"
+    exit 1
+fi
 
 cd "$OUTDIR"
+
+# Use git to clone the linux kernel source tree if it doesn’t exist in outdir.
+# Use the `--depth 1` command line argument with git if you’d like to minimize download time
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+    # Checkout the tag specified
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make defconfig
+    make kvm_guest.config
+    make -j$(nproc) Image
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+
+exit 
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
