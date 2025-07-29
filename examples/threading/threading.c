09f5ad14653d98e8a008758944b02de3cee043db
iolivares <4794288+iolivares@users.noreply.github.com>
2025-07-28 20:03:51 -0400

    assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,10 +10,29 @@
 
 void* threadfunc(void* thread_param)
 {
-
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct thread_data* data = (struct thread_data*) thread_param;
+    sleep(data->wait_to_obtain_ms / 1000.0);
+
+    if (pthread_mutex_lock(data->mutex) != 0) {
+        ERROR_LOG("Failed to lock mutex");
+        data->thread_complete_success = false;
+        return thread_param; // Return the thread_param for cleanup
+    }
+
+    sleep(data->wait_to_release_ms / 1000.0);
+
+    if (pthread_mutex_unlock(data->mutex) != 0) {
+        ERROR_LOG("Failed to unlock mutex");
+        data->thread_complete_success = false;
+        return thread_param; // Return the thread_param for cleanup
+    }
+
+    data->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +47,34 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    /* 
+     * Allocate memory for thread_data 
+     */
+    struct thread_data* data = (struct thread_data*) malloc(sizeof(struct thread_data));
+    if (!data) {
+        ERROR_LOG("Failed to allocate memory for thread_data");
+        return false;
+    }
+
+    /* 
+     * setup mutex and wait arguments 
+     */
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->mutex = mutex;
+    data->thread_complete_success = false;
+    
+    /* 
+     * Create thread and pass thread_data to created thread
+     * using threadfunc() as entry point.
+     */
+    if (pthread_create(thread, NULL, threadfunc, data) != 0) {
+        ERROR_LOG("Failed to create thread");
+        free(data);
+        return false;
+    }
+
+    return true;
 }
 
